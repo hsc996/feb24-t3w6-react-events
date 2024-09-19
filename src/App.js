5db40cb0d34973bb0ff5c8e1d259b8306ef3b564
hsc996 <han.scaife@gmail.com>
2024-09-19 15:46:08 +1000

    feat: finished with img warning

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import PokemonCard from './components/PokemonCard';
 
 class App extends React.Component {
   constructor(){
@@ -20,7 +21,7 @@ class App extends React.Component {
     console.log("Function log 2 running!");
   }
 
-  decreasePokemonNumber(){
+  decreasePokemonNumber = () => {
     if (this.state.numberofPokemon > 0){
       // need to use 'setState' if wishing to modify the state
     this.setState({
@@ -29,7 +30,7 @@ class App extends React.Component {
     }
   }
 
-  increasePokemonNumber(){
+  increasePokemonNumber= () => {
     this.setState({
       numberofPokemon: this.state.numberofPokemon + 1
     });
@@ -37,23 +38,33 @@ class App extends React.Component {
 
   render(){
     return(
-      // These bracket are a 'fragement'. This is allowed in React. Doesn't need to be a div.
       <>
-      <h1>Hello World</h1>
-      <p>Blah blah blah</p>
-      <h2>Number of Pokemon: {this.state.numberofPokemon}</h2>
-      <button onClick={ () => {
-        this.exampleLog1();
-        this.exampleLog2();
-        this.decreasePokemonNumber();
-      }}>
-        Decrease Pokemon Amount
-      </button>
-      <button onClick={() => {
-        this.increasePokemonNumber();
-      }}>
-        Increase Pokemon Amount
-      </button>
+        <h1>Hello World!</h1>
+        <p>Blah blah blah blah </p>
+        <h2>Number of Pokemon: {this.state.numberOfPokemon}</h2>
+        <button onClick={()=> {
+          this.exampleLog1();
+          this.exampleLog2();
+          this.decreasePokemonNumber();
+        }}>
+          Decrease Pokemon Amount 
+        </button>
+        <button onClick={this.increasePokemonNumber}>
+          Increase Pokemon Amount
+        </button>
+
+        {
+          // Create an array with a length equal to state
+          Array(this.state.numberOfPokemon)
+          .fill(null) // fill() just gives each item in the array something, so we can use it properly
+          // the actual part that does the loop or repeated action
+          .map((element, index) => {
+            // repeated action is in this block!
+            return <PokemonCard key={index} />
+          })
+        }
+        
+
       </>
     )
   }
